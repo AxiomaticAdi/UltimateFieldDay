f4ff05c52094a774b5241e4960674ad2525691b0
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-11-25 14:56:36 -0500

    Test change

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ export default function App() {
                             to="/games"
                             className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
                         >
-                            Start Planning!
+                            Start Planning!!
                         </Link>
                         <Link
                             to="#"
