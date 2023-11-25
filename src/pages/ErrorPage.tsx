ef7ae7459e401db8aebbea0fa773de8b7e1f3983
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-11-25 10:26:15 -0500

    Minor error page tweaks

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,17 +1,24 @@
 import { useRouteError } from "react-router-dom";
+import AppFrame from "../components/AppFrame";
 
 export default function ErrorPage() {
     const error = useRouteError();
     console.error(error);
 
     return (
-        <div className="flex flex-col items-center justify-center text-white">
-            <img src={"/ErrorPage.png"} width={"300px"} />
-            <h1>Oops!</h1>
-            <div>Sorry, an unexpected error has occurred.</div>
-            <div>
-                <i>{error.statusText || error.message}</i>
+        <AppFrame>
+            <div className="flex flex-col items-center justify-center gap-2 text-white">
+                <img
+                    className="rounded-full pt-16"
+                    src={"/ErrorPage.png"}
+                    width={"300px"}
+                />
+                <h1 className="text-4xl font-bold">Oops!</h1>
+                <div>Sorry, an unexpected error has occurred.</div>
+                <div>
+                    <i>{error.statusText || error.message}</i>
+                </div>
             </div>
-        </div>
+        </AppFrame>
     );
 }
