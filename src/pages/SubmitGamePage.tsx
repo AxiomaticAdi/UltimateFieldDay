95d4954591ffc8c2cd4bc9621bee331e44887e18
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-12-21 00:37:06 -0800

    Minor formatting change

--- src/pages/SubmitGamePage.tsx
+++ src/pages/SubmitGamePage.tsx
@@ -89,8 +89,7 @@ export default function SubmitGamePage() {
                                             type="text"
                                             name="gameTitle"
                                             id="gameTitle"
-                                            className="block border-0 bg-transparent py-1.5 pl-2 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                            // "block w-1/2 rounded-md border-0 bg-transparent py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            className="block rounded-md border-0 bg-transparent py-1.5 pl-3 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                             placeholder="Best Game Ever"
                                             value={gameTitle}
                                             onChange={(e) =>
