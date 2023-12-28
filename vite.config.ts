4fe32c5cd33236749c0ee987e6ffcb7866b6c63f
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-12-27 22:45:39 -0800

    Update vite server config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // TEST
-    // base:
-    //     window.location.hostname.indexOf("github") > -1
-    //         ? "/UltimateFieldDay/"
-    //         : "/",
+    server: {
+        port: 3000,
+    },
 });
