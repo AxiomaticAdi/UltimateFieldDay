ebb8d3d57d341f2a1d304f926225cabfe6f6f5d6
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-12-28 17:24:49 -0800

    Add game details page link in game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -122,7 +122,14 @@ export default function GameCardModal({ game }: { game: Game }) {
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="mt-5 sm:mt-6">
+                                    <div className="mt-5 flex gap-4 sm:mt-6">
+                                        <Link
+                                            className="inline-flex w-full justify-center rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                            to={"/games/" + game.gameId + "/"}
+                                        >
+                                            More info
+                                        </Link>
+
                                         <button
                                             type="button"
                                             className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
