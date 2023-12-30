f7d4fdd031b6ea1a6f6f11135335930c43c769c6
Adi <140542996+AxiomaticAdi@users.noreply.github.com>
2023-12-30 01:52:14 -0800

    Center text in short info sections

--- src/components/GameInfoSection.tsx
+++ src/components/GameInfoSection.tsx
@@ -7,14 +7,21 @@ export default function GameInfoSection({
     infoLabel,
     infoSection,
 }: GameInfoSectionProps) {
+    const isInfoCentered = infoSection.length < 75;
     return (
         <div className="flex flex-col justify-center text-left">
             <h2 className="text-center text-base font-semibold leading-7">
                 {infoLabel}
             </h2>
-            <p className="mt-1 text-base leading-6 text-gray-200">
-                {infoSection}
-            </p>
+            {isInfoCentered ? (
+                <p className="mt-1 text-center text-base leading-6 text-gray-200">
+                    {infoSection}
+                </p>
+            ) : (
+                <p className="mt-1 text-base leading-6 text-gray-200">
+                    {infoSection}
+                </p>
+            )}
         </div>
     );
 }
